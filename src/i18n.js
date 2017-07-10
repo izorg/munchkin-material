0f7e58dd29f91ae0550a52fd1fef39207957a31f
Viacheslav Zavoruev <izorg@yandex.ru>
2017-07-10 07:08:42 +0200

    Ukranian locale

--- src/i18n.js
+++ src/i18n.js
@@ -2,13 +2,16 @@ import { addLocaleData } from 'react-intl';
 
 import enLocaleData from 'react-intl/locale-data/en';
 import ruLocaleData from 'react-intl/locale-data/ru';
+import ukLocaleData from 'react-intl/locale-data/uk';
 
 import en from '../languages/en.json';
 import ru from '../languages/ru.json';
+import uk from '../languages/uk.json';
 
 const avaliableLocales = [
   'en',
   'ru',
+  'uk',
 ];
 
 const defaultLocale = 'en';
@@ -18,6 +21,7 @@ const LANGUAGE_LENGTH = 2;
 addLocaleData([
   ...enLocaleData,
   ...ruLocaleData,
+  ...ukLocaleData,
 ]);
 
 export const getLocale = () => {
@@ -32,6 +36,7 @@ export const getMessages = (locale) => {
   const allMessages = {
     en,
     ru,
+    uk,
   };
 
   return allMessages[locale] || allMessages[defaultLocale];
