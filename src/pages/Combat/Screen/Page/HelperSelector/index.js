ff80bd5ee915c69cb88754727b5f83a4eac73d59
Viacheslav Zavoruev <izorg@yandex.ru>
2018-02-09 11:25:22 +0100

    replace HelperSelector Route with match selector

---
+++ src/pages/Combat/Screen/Page/HelperSelector/index.js
@@ -0,0 +1,36 @@
+import { connect } from 'react-redux';
+import matchPath from 'react-router-dom/matchPath';
+import { goBack } from 'connected-react-router/lib/actions';
+import { createSelector, createStructuredSelector } from 'reselect';
+import { setCombatHelper } from 'munchkin-core/lib/actions';
+
+import Component from './Component';
+
+const open = createSelector(
+  (state) => state.router.location.pathname,
+  (pathname) =>
+    Boolean(
+      matchPath(pathname, {
+        exact: true,
+        path: '/player/:id/combat/add/helper',
+      }),
+    ),
+);
+
+const mapStateToProps = createStructuredSelector({
+  helpers: (state) =>
+    state.playerList
+      .filter((id) => id !== state.combat.playerId)
+      .map((id) => state.players[id]),
+  open,
+});
+
+const mapDispatchToProps = {
+  onClose: goBack,
+  onSelect: (id) => (dispatch) => {
+    dispatch(setCombatHelper(id));
+    dispatch(goBack());
+  },
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Component);
