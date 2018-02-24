24b54a9c7ba372b0d2d4f949fda9d4607e9ccc6f
Viacheslav Zavoruev <izorg@yandex.ru>
2018-02-24 10:19:35 +0100

    Multi select theme color support

--- src/pages/Home/Screen/index.jsx
+++ src/pages/Home/Screen/index.jsx
@@ -1,13 +1,22 @@
 import React, { Fragment } from 'react';
+import Helmet from 'react-helmet/lib/Helmet';
 import PropTypes from 'prop-types';
+import { withTheme } from 'material-ui/styles';
 
 import Transition from '../../../components/fab/Transition';
+
+import { MULTI } from '../modes';
 
 import Page from './Page';
 import PlayerAddButton from './PlayerAddButton';
 
-const HomeScreen = ({ match }) => (
+const HomeScreen = ({ match, theme }) => (
   <Fragment>
+    {match.params.mode === MULTI && (
+      <Helmet>
+        <meta name="theme-color" content={theme.palette.grey[100]} />
+      </Helmet>
+    )}
     <Page />
     <Transition
       appear={false}
@@ -26,4 +35,4 @@ HomeScreen.defaultProps = {
   match: null,
 };
 
-export default HomeScreen;
+export default withTheme()(HomeScreen);
