9a8f621379fdd61fd0c672b4125279a239e06a9c
Viacheslav Zavoruev <izorg@yandex.ru>
2018-02-24 10:21:37 +0100

    pure

--- src/pages/Combat/Screen/index.jsx
+++ src/pages/Combat/Screen/index.jsx
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, PureComponent } from 'react';
 import { hot } from 'react-hot-loader';
 import PropTypes from 'prop-types';
 import Slide from 'material-ui/transitions/Slide';
@@ -6,26 +6,32 @@ import Slide from 'material-ui/transitions/Slide';
 import Transition from '../../../components/fab/Transition';
 
 import HelperButton from './HelperButton';
-import HelperSelector from './Page/HelperSelector';
+import HelperSelector from './HelperSelector';
 import Page from './Page';
 
-const CombatScreen = ({ appear, match }) => (
-  <Fragment>
-    <Slide
-      appear={appear}
-      direction="left"
-      in={Boolean(match)}
-      mountOnEnter
-      unmountOnExit
-    >
-      <Page />
-    </Slide>
-    <Transition appear={appear} in={Boolean(match)}>
-      <HelperButton />
-    </Transition>
-    <HelperSelector />
-  </Fragment>
-);
+class CombatScreen extends PureComponent {
+  render() {
+    const { appear, match } = this.props;
+
+    return (
+      <Fragment>
+        <Slide
+          appear={appear}
+          direction="left"
+          in={Boolean(match)}
+          mountOnEnter
+          unmountOnExit
+        >
+          <Page />
+        </Slide>
+        <Transition appear={appear} in={Boolean(match)}>
+          <HelperButton />
+        </Transition>
+        <HelperSelector />
+      </Fragment>
+    );
+  }
+}
 
 CombatScreen.propTypes = {
   appear: PropTypes.bool.isRequired,
--- src/pages/Home/Screen/index.jsx
+++ src/pages/Home/Screen/index.jsx
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, PureComponent } from 'react';
 import Helmet from 'react-helmet/lib/Helmet';
 import PropTypes from 'prop-types';
 import { withTheme } from 'material-ui/styles';
@@ -10,22 +10,28 @@ import { MULTI } from '../modes';
 import Page from './Page';
 import PlayerAddButton from './PlayerAddButton';
 
-const HomeScreen = ({ match, theme }) => (
-  <Fragment>
-    {match.params.mode === MULTI && (
-      <Helmet>
-        <meta name="theme-color" content={theme.palette.grey[100]} />
-      </Helmet>
-    )}
-    <Page />
-    <Transition
-      appear={false}
-      in={Boolean(match) && match.isExact && !match.params.mode}
-    >
-      <PlayerAddButton />
-    </Transition>
-  </Fragment>
-);
+class HomeScreen extends PureComponent {
+  render() {
+    const { match, theme } = this.props;
+
+    return (
+      <Fragment>
+        {match.params.mode === MULTI && (
+          <Helmet>
+            <meta name="theme-color" content={theme.palette.grey[100]} />
+          </Helmet>
+        )}
+        <Page />
+        <Transition
+          appear={false}
+          in={Boolean(match) && match.isExact && !match.params.mode}
+        >
+          <PlayerAddButton />
+        </Transition>
+      </Fragment>
+    );
+  }
+}
 
 HomeScreen.propTypes = {
   match: PropTypes.object,
