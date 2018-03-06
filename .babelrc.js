8fe36ad18ae53b5cd4f9432e1a111c0b58ea945c
Viacheslav Zavoruev <izorg@yandex.ru>
2018-03-06 13:53:42 +0100

    .babelrc.js

---
+++ .babelrc.js
@@ -0,0 +1,39 @@
+module.exports = {
+  presets: ['@babel/preset-react'],
+  plugins: [
+    '@babel/plugin-proposal-object-rest-spread',
+    '@babel/plugin-syntax-dynamic-import',
+    '@babel/plugin-transform-modules-commonjs',
+    'lodash',
+    'version-inline',
+  ],
+  env: {
+    development: {
+      plugins: [
+        'react-hot-loader/babel',
+        [
+          'react-intl',
+          {
+            messagesDir: './messages/',
+          },
+        ],
+      ],
+    },
+    production: {
+      plugins: [
+        '@babel/plugin-transform-react-constant-elements',
+        '@babel/plugin-transform-react-inline-elements',
+        [
+          '@babel/plugin-transform-runtime',
+          {
+            polyfill: false,
+          },
+        ],
+        'transform-react-remove-prop-types',
+      ],
+    },
+    test: {
+      plugins: ['dynamic-import-node'],
+    },
+  },
+};
