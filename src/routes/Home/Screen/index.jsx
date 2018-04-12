f521e04be64243fabadc43849c18384e4ea9eb26
Viacheslav Zavoruev <izorg@yandex.ru>
2018-04-12 17:39:29 +0200

    Fix player button in single mode

--- src/routes/Home/Screen/index.jsx
+++ src/routes/Home/Screen/index.jsx
@@ -1,4 +1,5 @@
 import React, { Fragment, PureComponent } from 'react';
+import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 
 import Transition from '../../../components/fab/Transition';
@@ -8,14 +9,16 @@ import PlayerAddButton from './PlayerAddButton';
 
 class HomeScreen extends PureComponent {
   render() {
-    const { match } = this.props;
+    const { match, singleMode } = this.props;
 
     return (
       <Fragment>
         <Page />
         <Transition
           appear={false}
-          in={Boolean(match) && match.isExact && !match.params.mode}
+          in={
+            Boolean(match) && match.isExact && !match.params.mode && !singleMode
+          }
         >
           <PlayerAddButton />
         </Transition>
@@ -26,10 +29,16 @@ class HomeScreen extends PureComponent {
 
 HomeScreen.propTypes = {
   match: PropTypes.object,
+  singleMode: PropTypes.bool,
 };
 
 HomeScreen.defaultProps = {
   match: null,
+  singleMode: false,
 };
 
-export default HomeScreen;
+const mapStateToProps = (state) => ({
+  singleMode: state.app.singleMode,
+});
+
+export default connect(mapStateToProps)(HomeScreen);
