c6b0f3aae6fef1ce62be0a7bb7e2e90c41277bcb
Viacheslav Zavoruev <izorg@yandex.ru>
2018-04-14 10:19:00 +0200

    Better inProp selector

--- src/routes/Home/Screen/index.jsx
+++ src/routes/Home/Screen/index.jsx
@@ -1,4 +1,4 @@
-import React, { Fragment, PureComponent } from 'react';
+import React, { Fragment } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 
@@ -7,38 +7,32 @@ import Transition from '../../../components/fab/Transition';
 import Page from './Page';
 import PlayerAddButton from './PlayerAddButton';
 
-class HomeScreen extends PureComponent {
-  render() {
-    const { match, singleMode } = this.props;
-
-    return (
-      <Fragment>
-        <Page />
-        <Transition
-          appear={false}
-          in={
-            Boolean(match) && match.isExact && !match.params.mode && !singleMode
-          }
-        >
-          <PlayerAddButton />
-        </Transition>
-      </Fragment>
-    );
-  }
-}
+const HomeScreen = ({ in: inProp }) => (
+  <Fragment>
+    <Page />
+    <Transition appear={false} in={inProp}>
+      <PlayerAddButton />
+    </Transition>
+  </Fragment>
+);
 
 HomeScreen.propTypes = {
-  match: PropTypes.object,
-  singleMode: PropTypes.bool,
+  in: PropTypes.bool,
 };
 
 HomeScreen.defaultProps = {
-  match: null,
-  singleMode: false,
+  in: false,
 };
 
-const mapStateToProps = (state) => ({
-  singleMode: state.app.singleMode,
-});
+const mapStateToProps = (state, ownProps) => {
+  const {
+    app: { singleMode },
+  } = state;
+  const { match } = ownProps;
+
+  return {
+    in: Boolean(match) && match.isExact && !match.params.mode && !singleMode,
+  };
+};
 
 export default connect(mapStateToProps)(HomeScreen);
