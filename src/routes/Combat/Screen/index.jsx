9ddab201b95fe10b6df49b7ae3543c4ea5d9a99e
Viacheslav Zavoruev <izorg@yandex.ru>
2018-04-16 18:28:07 +0200

    Modal for focus trap

--- src/routes/Combat/Screen/index.jsx
+++ src/routes/Combat/Screen/index.jsx
@@ -1,34 +1,85 @@
-import React, { Fragment, PureComponent } from 'react';
+import React, { PureComponent } from 'react';
 import { hot } from 'react-hot-loader';
+import Transition from 'react-transition-group/Transition';
+import compose from 'recompose/compose';
 import PropTypes from 'prop-types';
+import Modal from 'material-ui/Modal';
 import Slide from 'material-ui/transitions/Slide';
+import { withStyles } from 'material-ui/styles';
 
-import Transition from '../../../components/fab/Transition';
+import FabTransition from '../../../components/fab/Transition';
 
 import HelperButton from './HelperButton';
 import HelperSelector from './HelperSelector';
 import Page from './Page';
 
+const styles = {
+  modal: {
+    zIndex: 1,
+  },
+
+  transition: {
+    height: '100%',
+    left: 0,
+    position: 'fixed',
+    top: 0,
+    width: '100%',
+  },
+};
+
 class CombatScreen extends PureComponent {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      appear: props.appear,
+    };
+
+    this.handleExited = this.handleExited.bind(this);
+  }
+
+  handleExited() {
+    if (!this.state.appear) {
+      this.setState({
+        appear: true,
+      });
+    }
+  }
+
   render() {
-    const { appear, match } = this.props;
+    const { classes, match, theme } = this.props;
+    const { appear } = this.state;
 
     return (
-      <Fragment>
-        <Slide
+      <Modal className={classes.modal} hideBackdrop open={Boolean(match)}>
+        <Transition
           appear={appear}
-          direction="left"
           in={Boolean(match)}
           mountOnEnter
+          onExited={this.handleExited}
+          timeout={{
+            enter: theme.transitions.duration.enteringScreen,
+            exit: theme.transitions.duration.leavingScreen,
+          }}
           unmountOnExit
         >
-          <Page />
-        </Slide>
-        <Transition appear={appear} in={Boolean(match)}>
-          <HelperButton />
+          <div className={classes.transition}>
+            <Slide
+              appear={appear}
+              direction="left"
+              in={Boolean(match)}
+              mountOnEnter
+              unmountOnExit
+            >
+              <Page />
+            </Slide>
+            <FabTransition appear={appear} in={Boolean(match)}>
+              <HelperButton />
+            </FabTransition>
+            <HelperSelector />
+          </div>
         </Transition>
-        <HelperSelector />
-      </Fragment>
+      </Modal>
     );
   }
 }
@@ -42,4 +93,6 @@ CombatScreen.defaultProps = {
   match: null,
 };
 
-export default hot(module)(CombatScreen);
+export default compose(hot(module), withStyles(styles, { withTheme: true }))(
+  CombatScreen,
+);
--- src/routes/Player/Screen/index.jsx
+++ src/routes/Player/Screen/index.jsx
@@ -1,20 +1,41 @@
-import React, { Fragment, PureComponent } from 'react';
+import React, { PureComponent } from 'react';
 import { hot } from 'react-hot-loader';
+import Transition from 'react-transition-group/Transition';
+import compose from 'recompose/compose';
 import PropTypes from 'prop-types';
+import Modal from 'material-ui/Modal';
 import Slide from 'material-ui/transitions/Slide';
+import { withStyles } from 'material-ui/styles';
 
-import Transition from '../../../components/fab/Transition';
+import FabTransition from '../../../components/fab/Transition';
 
 import CombatButton from './CombatButton';
 import Page from './Page';
+
+const styles = {
+  modal: {
+    zIndex: 1,
+  },
+
+  transition: {
+    height: '100%',
+    left: 0,
+    position: 'fixed',
+    top: 0,
+    width: '100%',
+  },
+};
 
 class PlayerScreen extends PureComponent {
   constructor(props) {
     super(props);
 
     this.state = {
+      appear: props.appear,
       playerId: props.match.params.id,
     };
+
+    this.handleExited = this.handleExited.bind(this);
   }
 
   getChildContext() {
@@ -33,24 +54,47 @@ class PlayerScreen extends PureComponent {
     }
   }
 
+  handleExited() {
+    if (!this.state.appear) {
+      this.setState({
+        appear: true,
+      });
+    }
+  }
+
   render() {
-    const { appear, match } = this.props;
+    const { classes, match, theme } = this.props;
+    const { appear } = this.state;
 
     return (
-      <Fragment>
-        <Slide
+      <Modal className={classes.modal} hideBackdrop open={Boolean(match)}>
+        <Transition
           appear={appear}
-          direction="left"
           in={Boolean(match)}
           mountOnEnter
+          onExited={this.handleExited}
+          timeout={{
+            enter: theme.transitions.duration.enteringScreen,
+            exit: theme.transitions.duration.leavingScreen,
+          }}
           unmountOnExit
         >
-          <Page />
-        </Slide>
-        <Transition appear={appear} in={Boolean(match) && match.isExact}>
-          <CombatButton />
+          <div className={classes.transition}>
+            <Slide
+              appear={appear}
+              direction="left"
+              in={Boolean(match)}
+              mountOnEnter
+              unmountOnExit
+            >
+              <Page />
+            </Slide>
+            <FabTransition appear={appear} in={Boolean(match) && match.isExact}>
+              <CombatButton />
+            </FabTransition>
+          </div>
         </Transition>
-      </Fragment>
+      </Modal>
     );
   }
 }
@@ -68,4 +112,6 @@ PlayerScreen.defaultProps = {
   match: null,
 };
 
-export default hot(module)(PlayerScreen);
+export default compose(hot(module), withStyles(styles, { withTheme: true }))(
+  PlayerScreen,
+);
