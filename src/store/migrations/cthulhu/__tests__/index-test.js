97fefbf0bd71b79a7937e1016483333c868ad465
Viacheslav Zavoruev <izorg@yandex.ru>
2018-08-05 12:10:19 +0200

    Removed old migrations

--- src/store/migrations/cthulhu/__tests__/index-test.js
+++
@@ -1,8 +0,0 @@
-import migrate from '../index';
-import state from './state.json';
-
-test('fix chtulhu to cthulhu', () => {
-  const { app } = migrate(state);
-
-  expect(app.theme).toBe('cthulhu');
-});
--- src/store/migrations/index.js
+++ src/store/migrations/index.js
@@ -1,6 +1,1 @@
-import cthulhu from './cthulhu';
-import playerColors from './playerColors';
-import sex from './sex';
-import theme from './theme';
-
-export default [playerColors, theme, cthulhu, sex];
+export default [];
