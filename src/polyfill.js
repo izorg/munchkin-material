44bb3a108ede3f4564c512d9d36955d0ab7b97e6
Viacheslav Zavoruev <izorg@yandex.ru>
2018-11-08 21:06:57 +0100

    Android Browser 4.4 polyfill added

--- src/polyfill.js
+++ src/polyfill.js
@@ -2,6 +2,7 @@ import 'core-js/es6/array';
 import 'core-js/es6/map';
 import 'core-js/es6/object';
 import 'core-js/es6/set';
+import 'core-js/es6/string';
 import 'core-js/es6/promise';
 import 'core-js/es6/symbol';
 import 'core-js/es6/weak-map';
