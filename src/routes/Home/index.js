254f6563bcca4e416103f2be2dc3136cecefd343
Viacheslav Zavoruev <izorg@yandex.ru>
2018-11-20 07:27:39 +0100

    Remove commented code

--- src/routes/Home/index.js
+++ src/routes/Home/index.js
@@ -4,17 +4,6 @@ import { get } from 'lodash/fp';
 
 import Component from './Component';
 
-// const mapStateToProps = (state, ownProps) => {
-//   const {
-//     app: { singleMode },
-//   } = state;
-//   const { match } = ownProps;
-//
-//   return {
-//     in: Boolean(match) && match.isExact && !match.params.mode && !singleMode,
-//   };
-// };
-
 const mode = (state, ownProps) => get(['match', 'params', 'mode'], ownProps);
 
 const mapStateToProps = createStructuredSelector({
