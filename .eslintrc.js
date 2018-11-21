ad8e8a315fd78b7ea69f937203a945d99cfd1ddb
Viacheslav Zavoruev <izorg@yandex.ru>
2018-11-21 22:21:50 +0100

    Packages upgrade

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,20 +2,41 @@ const prettier = require('./.prettierrc');
 
 module.exports = {
   parser: 'babel-eslint',
-  extends: ['airbnb', 'plugin:jest/recommended', 'prettier'],
+  extends: [
+    'airbnb',
+    'plugin:jest/recommended',
+    'plugin:prettier/recommended',
+    'prettier/react',
+  ],
   env: {
     browser: true,
   },
   plugins: ['json', 'prettier'],
   rules: {
-    'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
-    'no-empty': ['error', { allowEmptyCatch: true }],
+    'import/no-extraneous-dependencies': [
+      'error',
+      {
+        devDependencies: true,
+      },
+    ],
+    'no-empty': [
+      'error',
+      {
+        allowEmptyCatch: true,
+      },
+    ],
     'prettier/prettier': ['error', prettier],
-    'react/forbid-prop-types': [true, { forbid: ['match'] }],
-    'react/jsx-wrap-multilines': 'off',
+    'react/forbid-prop-types': [
+      'error',
+      {
+        forbid: ['match'],
+      },
+    ],
     'react/prop-types': [
       'error',
-      { ignore: ['classes', 'className', 'theme'] },
+      {
+        ignore: ['classes', 'className', 'theme'],
+      },
     ],
   },
 };
