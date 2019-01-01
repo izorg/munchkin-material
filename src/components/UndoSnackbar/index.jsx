4728612fb66d1eb49f122e0575912cff3ff768be
Viacheslav Zavoruev <izorg@yandex.ru>
2019-01-01 18:15:42 +0100

    Undo message preserving

--- src/components/UndoSnackbar/index.jsx
+++ src/components/UndoSnackbar/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { FormattedMessage } from 'react-intl';
 import { compose } from 'recompose';
 import PropTypes from 'prop-types';
@@ -22,24 +22,53 @@ const styles = (theme) => ({
   },
 });
 
-const Undo = ({ classes, className, onClose, width, ...rest }) => (
-  <Snackbar
-    action={
-      <Button color="primary" onClick={(event) => onClose(event, 'undo')}>
-        <FormattedMessage id="undo" defaultMessage="Undo" />
-      </Button>
+class Undo extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+  }
+
+  static getDerivedStateFromProps(nextProps) {
+    const { children, message } = nextProps;
+
+    if (children || message) {
+      return {
+        children,
+        message,
+      };
     }
-    anchorOrigin={{
-      horizontal: 'left',
-      vertical: 'bottom',
-    }}
-    className={cns(classes.root, className)}
-    ContentProps={{ className: classes.content }}
-    onClose={onClose}
-    TransitionComponent={isWidthDown('sm', width) ? Fade : undefined}
-    {...rest}
-  />
-);
+
+    return null;
+  }
+
+  render() {
+    const { classes, className, onClose, width, ...rest } = this.props;
+    const { children, message } = this.state;
+
+    return (
+      <Snackbar
+        action={
+          <Button color="primary" onClick={(event) => onClose(event, 'undo')}>
+            <FormattedMessage id="undo" defaultMessage="Undo" />
+          </Button>
+        }
+        anchorOrigin={{
+          horizontal: 'left',
+          vertical: 'bottom',
+        }}
+        className={cns(classes.root, className)}
+        ContentProps={{ className: classes.content }}
+        onClose={onClose}
+        TransitionComponent={isWidthDown('sm', width) ? Fade : undefined}
+        {...rest}
+        message={message}
+      >
+        {children}
+      </Snackbar>
+    );
+  }
+}
 
 Undo.propTypes = {
   onClose: PropTypes.func.isRequired,
