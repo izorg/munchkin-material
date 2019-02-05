f08537d04fc954d62378d0d9540ada26c8b970d3
Viacheslav Zavoruev <izorg@yandex.ru>
2019-02-05 20:33:06 +0100

    Fix helper dialog show

--- src/routes/Combat/HelperButton/Action/index.jsx
+++ src/routes/Combat/HelperButton/Action/index.jsx
@@ -1,52 +1,18 @@
-import React, { Component, createRef } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
-import { RootRef } from '@material-ui/core';
 import { SpeedDialAction } from '@material-ui/lab';
-import Hammer from 'hammerjs';
-import { delay, noop } from 'lodash/fp';
+import { noop } from 'lodash/fp';
 
-class HelperButtonAction extends Component {
-  constructor(props) {
-    super(props);
-
-    this.buttonRef = createRef();
-  }
-
-  componentDidMount() {
-    this.addHammer();
-  }
-
-  componentWillUnmount() {
-    this.removeHammer();
-  }
-
-  addHammer() {
-    const { onClick } = this.props;
-
-    this.hammer = new Hammer(this.buttonRef.current, {
-      recognizers: [[Hammer.Tap]],
-    });
-
-    this.hammer.on('tap', () => delay(10, onClick));
-  }
-
-  removeHammer() {
-    if (this.hammer) {
-      this.hammer.stop();
-      this.hammer.destroy();
-
-      this.hammer = null;
-    }
-  }
-
-  render() {
-    return (
-      <RootRef rootRef={this.buttonRef}>
-        <SpeedDialAction {...this.props} onClick={noop} />
-      </RootRef>
-    );
-  }
-}
+const HelperButtonAction = ({ onClick, ...rest }) => (
+  <SpeedDialAction
+    {...rest}
+    ButtonProps={{
+      // eslint-disable-next-line react/button-has-type
+      component: (props) => <button {...props} onClick={onClick} />,
+    }}
+    onClick={noop}
+  />
+);
 
 HelperButtonAction.propTypes = {
   onClick: PropTypes.func.isRequired,
