b6966f8ffa1ca553cfbe7de4f4e33c21d968096f
Viacheslav Zavoruev <izorg@yandex.ru>
2019-02-28 01:37:34 +0100

    Replace reducers folder with file

--- src/reducers.js
+++ src/reducers.js
@@ -5,12 +5,12 @@ import {
   versionsReducer,
 } from 'munchkin-core';
 
-import app from '../ducks/app';
-import dice from '../ducks/dice';
-import playerList from '../ducks/playerList';
-import theme from '../ducks/theme';
-import undo from '../ducks/undo';
-import update from '../ducks/update';
+import app from './ducks/app';
+import dice from './ducks/dice';
+import playerList from './ducks/playerList';
+import theme from './ducks/theme';
+import undo from './ducks/undo';
+import update from './ducks/update';
 
 export default {
   app,
