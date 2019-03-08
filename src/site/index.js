fd8364782affa72f0d63d334ccffdf9f6256475a
Viacheslav Zavoruev <izorg@yandex.ru>
2019-03-08 23:43:44 +0100

    Use Sentry only on prod

--- src/site/index.js
+++ src/site/index.js
@@ -8,16 +8,21 @@ import init from '../index';
 import registerServiceWorker from './registerServiceWorker';
 
 const dev = process.env.NODE_ENV === 'development';
+const prod = process.env.NODE_ENV === 'production';
+
 const el = document.getElementById('app');
+
 const history = createHistory();
 
 const { host, pathname, protocol } = window.location;
 
-Sentry.init({
-  dsn: 'https://41e93153dfb94d9db3ed8a2cbc7228a9@sentry.io/253536',
-  environment: process.env.NODE_ENV,
-  release: VERSION,
-});
+if (prod) {
+  Sentry.init({
+    dsn: 'https://41e93153dfb94d9db3ed8a2cbc7228a9@sentry.io/253536',
+    environment: process.env.NODE_ENV,
+    release: VERSION,
+  });
+}
 
 const app = init(el, {
   history,
