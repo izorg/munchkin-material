e9938988a64fbc189fefb33d2713017d7dd2e0dc
Viacheslav Zavoruev <izorg@yandex.ru>
2019-04-01 01:33:25 +0200

    Disable jest notification

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  notify: true,
+  notify: false, // waiting for https://github.com/facebook/jest/issues/8036
   setupFilesAfterEnv: ['./jest.setup.js'],
   testURL: 'http://localhost',
 };
