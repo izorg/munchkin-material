245821b14936ac55f7d4950639853cda293a4f26
Viacheslav Zavoruev <izorg@yandex.ru>
2019-03-31 11:18:40 +0200

    Reuse open selector

--- src/components/menu/Drawer/index.js
+++ src/components/menu/Drawer/index.js
@@ -1,25 +1,27 @@
 import { goBack, push } from 'connected-react-router';
 import { connect } from 'react-redux';
-import { createStructuredSelector } from 'reselect';
+import { createSelector, createStructuredSelector } from 'reselect';
 import { flow, get, isNull } from 'lodash/fp';
 
 import { getQuery, stringifyQuery } from '../../../utils/location';
 
 import Component from './Component';
 
-const mapStateToProps = createStructuredSelector({
-  open: flow(
-    getQuery,
+const open = createSelector(
+  getQuery,
+  flow(
     get('menu'),
     isNull,
   ),
+);
+
+const mapStateToProps = createStructuredSelector({
+  open,
 });
 
 const mapDispatchToProps = {
   onClose: () => (dispatch, getState) => {
-    const search = getQuery(getState());
-
-    if (search.menu !== undefined) {
+    if (open(getState())) {
       dispatch(goBack());
     }
   },
