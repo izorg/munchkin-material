f8ad8b1fd54c0e48c88af7778e5f38b232b2bb6b
Viacheslav Zavoruev <izorg@yandex.ru>
2019-05-26 00:10:35 +0200

    Convert <CombatMonsterSlider /> to function

--- src/routes/Combat/MonsterSlider/Component.jsx
+++ src/routes/Combat/MonsterSlider/Component.jsx
@@ -1,173 +1,154 @@
-import React, { Component } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { FormattedMessage } from 'react-intl';
 import MediaQuery from 'react-responsive';
 import SwipeableViews from 'react-swipeable-views';
 import PropTypes from 'prop-types';
-import { IconButton, Paper, withStyles } from '@material-ui/core';
+import { IconButton, makeStyles, Paper } from '@material-ui/core';
 import { CloseCircle } from 'mdi-material-ui';
 import clsx from 'clsx';
 
 import Monster from './Monster';
 
-const styles = (theme) => ({
-  monsters: {
-    alignItems: 'flex-end',
-    display: 'flex',
-    position: 'relative',
-  },
-
-  remove: {
-    position: 'absolute !important',
-    right: 8,
-    top: 8,
-  },
-
-  [`${theme.breakpoints.up('sm')} and (orientation: portrait)`]: {
-    paper: {
-      marginBottom: 8,
-    },
-  },
-
-  '@media (orientation: landscape)': {
+const useStyles = makeStyles(
+  (theme) => ({
     monsters: {
-      alignItems: 'center',
+      alignItems: 'flex-end',
+      display: 'flex',
+      position: 'relative',
     },
 
     remove: {
-      bottom: 8,
-      left: 8,
-      right: 'auto',
-      top: 'auto',
+      position: 'absolute !important',
+      right: 8,
+      top: 8,
     },
-  },
-});
 
-class CombatMonsterSlider extends Component {
-  constructor(props) {
-    super(props);
+    [`${theme.breakpoints.up('sm')} and (orientation: portrait)`]: {
+      paper: {
+        marginBottom: 8,
+      },
+    },
 
-    this.state = {
-      index: 0,
-    };
+    '@media (orientation: landscape)': {
+      monsters: {
+        alignItems: 'center',
+      },
 
-    this.handleChangeIndex = this.handleChangeIndex.bind(this);
-  }
+      remove: {
+        bottom: 8,
+        left: 8,
+        right: 'auto',
+        top: 'auto',
+      },
+    },
+  }),
+  { name: 'CombatMonsterSlider' },
+);
 
-  componentDidUpdate(prevProps) {
-    const { monsters } = this.props;
+const CombatMonsterSlider = ({ className, monsters, onMonsterRemove }) => {
+  const classes = useStyles();
 
-    if (monsters.length > prevProps.monsters.length) {
-      // eslint-disable-next-line react/no-did-update-set-state
-      this.setState({
-        index: monsters.length - 1,
-      });
+  const [index, setIndex] = useState(0);
+  const monsterCount = useRef(monsters.length);
+
+  useEffect(() => {
+    if (monsters.length > monsterCount.current) {
+      setIndex(monsters.length - 1);
     }
-  }
 
-  handleChangeIndex(index) {
-    this.setState({
-      index,
-    });
-  }
+    monsterCount.current = monsters.length;
+  }, [monsters.length]);
 
-  handleRemove(monsterId) {
-    const { monsters, onMonsterRemove } = this.props;
-    const index = monsters.indexOf(monsterId);
+  const handleRemove = (monsterId) => {
+    const monsterIndex = monsters.indexOf(monsterId);
 
-    if (index > 0) {
-      this.setState({
-        index: index - 1,
-      });
+    if (monsterIndex > 0) {
+      setIndex(monsterIndex - 1);
     }
 
     onMonsterRemove(monsterId);
-  }
+  };
 
-  render() {
-    const { classes, className, monsters } = this.props;
-    const { index } = this.state;
+  const views = monsters.map((id, monsterIndex) => (
+    <Paper key={id} className={classes.paper}>
+      <Monster
+        id={id}
+        title={
+          <FormattedMessage
+            defaultMessage="Monster {number}"
+            id="combat.monster"
+            values={{
+              number: monsterIndex + 1,
+            }}
+          />
+        }
+      />
 
-    const views = monsters.map((id, monsterIndex) => (
-      <Paper key={id} className={classes.paper}>
-        <Monster
-          id={id}
-          title={
-            <FormattedMessage
-              defaultMessage="Monster {number}"
-              id="combat.monster"
-              values={{
-                number: monsterIndex + 1,
-              }}
-            />
-          }
-        />
+      {monsters.length > 1 && monsterIndex === index && (
+        <IconButton
+          className={classes.remove}
+          onClick={() => handleRemove(id)}
+          style={{
+            width: 36,
+            height: 36,
+            padding: 6,
+          }}
+        >
+          <CloseCircle />
+        </IconButton>
+      )}
+    </Paper>
+  ));
 
-        {monsters.length > 1 && monsterIndex === index && (
-          <IconButton
-            className={classes.remove}
-            onClick={() => this.handleRemove(id)}
-            style={{
-              width: 36,
-              height: 36,
-              padding: 6,
-            }}
-          >
-            <CloseCircle />
-          </IconButton>
-        )}
-      </Paper>
-    ));
+  return (
+    <div className={clsx(classes.monsters, className)}>
+      <MediaQuery orientation="portrait">
+        <SwipeableViews
+          enableMouseEvents
+          index={index}
+          onChangeIndex={(value) => setIndex(value)}
+          slideStyle={{
+            padding: '8px 8px 0',
+            position: 'relative',
+          }}
+          style={{
+            flex: 1,
+            padding: '0 48px',
+          }}
+        >
+          {views}
+        </SwipeableViews>
+      </MediaQuery>
 
-    return (
-      <div className={clsx(classes.monsters, className)}>
-        <MediaQuery orientation="portrait">
-          <SwipeableViews
-            enableMouseEvents
-            index={index}
-            onChangeIndex={this.handleChangeIndex}
-            slideStyle={{
-              padding: '8px 8px 0',
-              position: 'relative',
-            }}
-            style={{
-              flex: 1,
-              padding: '0 48px',
-            }}
-          >
-            {views}
-          </SwipeableViews>
-        </MediaQuery>
-
-        <MediaQuery orientation="landscape">
-          <SwipeableViews
-            axis="y"
-            containerStyle={{
-              height: 221,
-              width: '100%',
-            }}
-            enableMouseEvents
-            ignoreNativeScroll
-            index={index}
-            onChangeIndex={this.handleChangeIndex}
-            slideStyle={{
-              height: 221,
-              padding: '8px 48px 8px 8px',
-              position: 'relative',
-            }}
-            style={{
-              alignItems: 'center',
-              display: 'flex',
-              overflowY: 'visible',
-              width: '100%',
-            }}
-          >
-            {views}
-          </SwipeableViews>
-        </MediaQuery>
-      </div>
-    );
-  }
-}
+      <MediaQuery orientation="landscape">
+        <SwipeableViews
+          axis="y"
+          containerStyle={{
+            height: 221,
+            width: '100%',
+          }}
+          enableMouseEvents
+          ignoreNativeScroll
+          index={index}
+          onChangeIndex={(value) => setIndex(value)}
+          slideStyle={{
+            height: 221,
+            padding: '8px 48px 8px 8px',
+            position: 'relative',
+          }}
+          style={{
+            alignItems: 'center',
+            display: 'flex',
+            overflowY: 'visible',
+            width: '100%',
+          }}
+        >
+          {views}
+        </SwipeableViews>
+      </MediaQuery>
+    </div>
+  );
+};
 
 CombatMonsterSlider.propTypes = {
   monsters: PropTypes.arrayOf(PropTypes.string),
@@ -178,4 +159,4 @@ CombatMonsterSlider.defaultProps = {
   monsters: [],
 };
 
-export default withStyles(styles)(CombatMonsterSlider);
+export default CombatMonsterSlider;
