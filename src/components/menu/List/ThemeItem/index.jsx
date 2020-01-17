b70dabae02a739503e989052d6c55e9c6d3bc902
Viacheslav Zavoruev <izorg@yandex.ru>
2020-01-17 22:45:00 +0100

    <ThemeItem /> refactor

--- src/components/menu/List/ThemeItem/index.jsx
+++ src/components/menu/List/ThemeItem/index.jsx
@@ -1,12 +1,16 @@
 import { ListItemIcon, makeStyles, useTheme } from '@material-ui/core';
 import clsx from 'clsx';
+import { push, replace } from 'connected-react-router';
 import { Palette } from 'mdi-material-ui';
 import React from 'react';
 import { useIntl } from 'react-intl';
+import { useDispatch, useSelector } from 'react-redux';
 
 import { names as themeNames } from '../../../../styles/themes';
+import { stringifyQuery } from '../../../../utils/location';
 
 import themeMessages from '../../../theme/messages';
+import openSelector from '../../openSelector';
 
 import ListItem from '../Item';
 import ListItemText from '../ItemText';
@@ -23,11 +27,29 @@ const useStyles = makeStyles(
 
 const ThemeItem = ({ className, ...rest }) => {
   const classes = useStyles();
+  const dispatch = useDispatch();
   const intl = useIntl();
   const theme = useTheme();
 
+  const open = useSelector(openSelector);
+
+  const onClick = () => {
+    const location = { search: stringifyQuery({ theme: null }) };
+
+    if (open) {
+      dispatch(replace(location));
+    } else {
+      dispatch(push(location));
+    }
+  };
+
   return (
-    <ListItem button className={clsx(className, classes.root)} {...rest}>
+    <ListItem
+      button
+      className={clsx(className, classes.root)}
+      onClick={onClick}
+      {...rest}
+    >
       <ListItemIcon>
         <Palette style={{ color: theme.palette.primary.main }} />
       </ListItemIcon>
