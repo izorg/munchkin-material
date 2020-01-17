ac8a77458e9896ec6cca50fae0f52c40920c80ae
Viacheslav Zavoruev <izorg@yandex.ru>
2020-01-17 22:55:16 +0100

    <VersionItem /> refactor

---
+++ src/components/menu/List/VersionItem/index.jsx
@@ -0,0 +1,34 @@
+import { ListItemIcon } from '@material-ui/core';
+import { goBack } from 'connected-react-router';
+import { CellphoneArrowDown, InformationOutline } from 'mdi-material-ui';
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
+import { applyUpdate } from '../../../../ducks/update';
+
+import ListItem from '../Item';
+import ListItemText from '../ItemText';
+
+const VersionItem = (props) => {
+  const dispatch = useDispatch();
+
+  const update = useSelector((state) => state.update);
+
+  const onClick = () => {
+    if (update) {
+      dispatch(goBack());
+      dispatch(applyUpdate());
+    }
+  };
+
+  return (
+    <ListItem button onClick={onClick} {...props}>
+      <ListItemIcon>
+        {update ? <CellphoneArrowDown /> : <InformationOutline />}
+      </ListItemIcon>
+      <ListItemText primary={VERSION} />
+    </ListItem>
+  );
+};
+
+export default VersionItem;
