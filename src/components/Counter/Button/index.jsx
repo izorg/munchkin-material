c9f738b10c4ce962a87b3f7e5f6efbb08cb7fa1c
Viacheslav Zavoruev <izorg@yandex.ru>
2020-04-29 07:40:53 +0200

    Tap & hold to trigger counter

--- src/components/Counter/Button/index.jsx
+++ src/components/Counter/Button/index.jsx
@@ -1,33 +1,80 @@
 import { IconButton } from '@material-ui/core';
 import PropTypes from 'prop-types';
-import React from 'react';
+import React, { useCallback, useEffect, useRef } from 'react';
 import { useDrag } from 'react-use-gesture';
 
 const displayName = 'CounterButton';
 
-const touch = 'ontouchstart' in window;
+const CounterButton = (props) => {
+  const { disabled, onClick: onClickProp } = props;
 
-const CounterButton = ({ onClick, ...rest }) => {
-  const bind = useDrag(
-    (state) => {
-      const { tap } = state;
+  const timeoutRef = useRef(0);
+  const intervalRef = useRef(0);
+  const ignoreClickRef = useRef(false);
 
-      if (tap) {
-        onClick();
+  const clearPress = useCallback(() => {
+    if (timeoutRef.current) {
+      clearTimeout(timeoutRef.current);
+
+      timeoutRef.current = 0;
+    }
+
+    if (intervalRef.current) {
+      clearInterval(intervalRef.current);
+
+      intervalRef.current = 0;
+    }
+  }, []);
+
+  useEffect(() => () => clearPress(), [clearPress]);
+  useEffect(() => {
+    if (disabled) {
+      clearPress();
+    }
+  }, [clearPress, disabled]);
+
+  const onClick = () => {
+    if (ignoreClickRef.current) {
+      ignoreClickRef.current = false;
+    } else {
+      onClickProp();
+    }
+  };
+
+  const bind = useDrag((state) => {
+    const { first, tap } = state;
+
+    if (first) {
+      ignoreClickRef.current = false;
+
+      timeoutRef.current = setTimeout(() => {
+        intervalRef.current = setInterval(() => onClickProp(), 250);
+      }, 500);
+    }
+
+    if (tap) {
+      ignoreClickRef.current = true;
+
+      if (!intervalRef.current && !disabled) {
+        onClickProp();
       }
-    },
-    {
-      enabled: touch,
-    },
-  );
+    }
 
-  return (
-    <IconButton onClick={touch ? undefined : onClick} {...rest} {...bind()} />
-  );
+    if (!first) {
+      clearPress();
+    }
+  });
+
+  return <IconButton {...props} {...bind()} onClick={onClick} />;
 };
 
 CounterButton.propTypes = {
+  disabled: PropTypes.bool,
   onClick: PropTypes.func.isRequired,
+};
+
+CounterButton.defaultProps = {
+  disabled: false,
 };
 
 CounterButton.displayName = displayName;
