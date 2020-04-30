de8d78974283836175970134e108801c80be93d1
Viacheslav Zavoruev <izorg@yandex.ru>
2020-04-30 08:27:21 +0200

    Fix passive listener

--- src/components/Counter/Button/index.jsx
+++ src/components/Counter/Button/index.jsx
@@ -6,6 +6,7 @@ import { useDrag } from 'react-use-gesture';
 const displayName = 'CounterButton';
 
 const CounterButton = ({ disabled, onClick, ...rest }) => {
+  const ref = useRef();
   const timeoutRef = useRef(0);
   const intervalRef = useRef(0);
 
@@ -30,27 +31,39 @@ const CounterButton = ({ disabled, onClick, ...rest }) => {
     }
   }, [clearPress, disabled]);
 
-  const bind = useDrag((state) => {
-    const { event, first, tap } = state;
+  const bind = useDrag(
+    (state) => {
+      const { event, first, tap } = state;
 
-    if (first && !timeoutRef.current && !intervalRef.current) {
-      event.preventDefault();
+      if (first && !timeoutRef.current && !intervalRef.current) {
+        event.preventDefault();
 
-      timeoutRef.current = setTimeout(() => {
-        intervalRef.current = setInterval(() => onClick(), 250);
-      }, 500);
-    }
-
-    if (tap) {
-      if (!intervalRef.current && !disabled) {
-        onClick();
+        timeoutRef.current = setTimeout(() => {
+          intervalRef.current = setInterval(() => onClick(), 250);
+        }, 500);
       }
-    }
 
-    if (!first) {
-      clearPress();
-    }
-  });
+      if (tap) {
+        if (!intervalRef.current && !disabled) {
+          onClick();
+        }
+      }
+
+      if (!first) {
+        clearPress();
+      }
+    },
+    {
+      domTarget: ref,
+      eventOptions: {
+        passive: false,
+      },
+    },
+  );
+
+  useEffect(() => {
+    bind();
+  }, [bind]);
 
   const onKeyDown = ({ key }) => {
     if (key === ' ' || key === 'Enter') {
@@ -59,12 +72,7 @@ const CounterButton = ({ disabled, onClick, ...rest }) => {
   };
 
   return (
-    <IconButton
-      disabled={disabled}
-      {...rest}
-      {...bind()}
-      onKeyDown={onKeyDown}
-    />
+    <IconButton ref={ref} disabled={disabled} {...rest} onKeyDown={onKeyDown} />
   );
 };
 
