df0359f65406190e9701ceb86b441be57c4e195f
Viacheslav Zavoruev <izorgster@gmail.com>
2020-07-03 07:41:51 +0200

    Better handle source map for cordova

--- src/sentry.js
+++ src/sentry.js
@@ -1,15 +1,30 @@
 import * as Sentry from '@sentry/browser';
-import { ExtraErrorData } from '@sentry/integrations';
+import { ExtraErrorData, RewriteFrames } from '@sentry/integrations';
 
 const sentry = (build, dsn) => {
+  let integrations = [
+    new ExtraErrorData({
+      depth: 10,
+    }),
+  ];
+
+  if (build === 'cordova') {
+    integrations = [
+      ...integrations,
+      new RewriteFrames({
+        iteratee: (frame) => {
+          frame.filename = frame.filename.replace(/^.*\/www\//, '/');
+
+          return frame;
+        },
+      }),
+    ];
+  }
+
   Sentry.init({
     dsn,
     environment: process.env.NODE_ENV,
-    integrations: [
-      new ExtraErrorData({
-        depth: 10,
-      }),
-    ],
+    integrations,
     normalizeDepth: 11,
     release: `${VERSION}-${build}`,
   });
