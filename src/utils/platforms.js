dadc8e4d9f97c66755dfe1cb8c249b537c467bca
Viacheslav Zavoruev <izorgster@gmail.com>
2021-03-18 08:06:58 +0100

    Remove unused platform detections

--- src/utils/platforms.js
+++ src/utils/platforms.js
@@ -1,7 +1,3 @@
-const { platform, userAgent } = navigator;
+const { platform } = navigator;
 
 export const ios = platform === "iPhone" || platform === "iPad";
-export const android = userAgent.includes("Android");
-
-export const edge = userAgent.includes("Edge");
-export const ie = userAgent.includes("Trident");
