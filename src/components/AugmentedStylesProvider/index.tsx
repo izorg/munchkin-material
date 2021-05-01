80d34aabc8b3a479e81f3c5165236449a855a844
Viacheslav Zavoruev <izorgster@gmail.com>
2021-05-01 21:23:37 +0200

    Convert <AugmentedStylesProvider /> in TS

--- src/components/AugmentedStylesProvider/index.tsx
+++ src/components/AugmentedStylesProvider/index.tsx
@@ -2,35 +2,37 @@ import createCache from "@emotion/cache";
 import { CacheProvider } from "@emotion/react";
 import { StylesProvider } from "@material-ui/core";
 import PropTypes from "prop-types";
+import type { FC } from "react";
 import { useIntl } from "react-intl";
 import { prefixer } from "stylis";
 import rtlPlugin from "stylis-plugin-rtl";
 
 import { getDirection } from "../../i18n";
 
-const displayName = "AugmentedStylesProvider";
-
 const ltrCache = createCache({
   key: "ltr",
   prepend: true,
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
   stylisPlugins: [prefixer],
 });
 
 const rtlCache = createCache({
   key: "rtl",
   prepend: true,
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
   stylisPlugins: [prefixer, rtlPlugin],
 });
 
-const AugmentedStylesProvider = ({ children }) => {
+const AugmentedStylesProvider: FC = ({ children }) => {
   const { locale } = useIntl();
   const direction = getDirection(locale);
+  const cache = direction === "rtl" ? rtlCache : ltrCache;
 
   return (
     <StylesProvider injectFirst>
-      <CacheProvider value={direction === "rtl" ? rtlCache : ltrCache}>
-        {children}
-      </CacheProvider>
+      <CacheProvider value={cache}>{children}</CacheProvider>
     </StylesProvider>
   );
 };
@@ -38,7 +40,5 @@ const AugmentedStylesProvider = ({ children }) => {
 AugmentedStylesProvider.propTypes = {
   children: PropTypes.node,
 };
-
-AugmentedStylesProvider.displayName = displayName;
 
 export default AugmentedStylesProvider;
