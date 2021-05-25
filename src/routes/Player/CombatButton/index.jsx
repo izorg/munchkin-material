ab3180f75c22791849f1f7ae56ceaa0dddd0e931
Viacheslav Zavoruev <izorgster@gmail.com>
2021-05-25 15:20:29 +0200

    No need to pay for combat

--- src/routes/Player/CombatButton/index.jsx
+++ src/routes/Player/CombatButton/index.jsx
@@ -3,33 +3,20 @@ import PropTypes from "prop-types";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 
-import { useFullVersion } from "../../../components/FullVersionProvider";
 import ScreenFab from "../../../components/ScreenFab";
 import { startCombat } from "../../../ducks/combat";
 
 const displayName = "CombatButton";
 
 const CombatButton = ({ playerId, ...rest }) => {
-  const { cordova } = window;
-
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
   const combatFinished = useSelector((state) => state.present.combat.finished);
   const combatPlayerId = useSelector((state) => state.present.combat.playerId);
 
-  const { buyFullVersion, fullVersion } = useFullVersion();
-
   const goToCombat = async () => {
     if (combatFinished || playerId !== combatPlayerId) {
-      if (!fullVersion && cordova?.platformId !== "ios") {
-        try {
-          await buyFullVersion();
-        } catch (error) {
-          return;
-        }
-      }
-
       dispatch(startCombat(playerId));
     }
 
