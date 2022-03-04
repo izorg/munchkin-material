7bde2bb7a9f6fca1126020390635c32a9af0facd
Viacheslav Zavoruev <izorgster@gmail.com>
2022-03-05 00:18:14 +0100

    Migrate MenuListItemText to sx prop

--- src/components/menu/List/ItemText/index.tsx
+++ src/components/menu/List/ItemText/index.tsx
@@ -1,14 +1,13 @@
-import { css } from "@emotion/react";
 import { ListItemText, type ListItemTextProps } from "@mui/material";
 
 const MenuListItemText = (props: ListItemTextProps) => (
   <ListItemText
-    css={css`
-      overflow: hidden;
-      text-overflow: ellipsis;
-    `}
     primaryTypographyProps={{ noWrap: true }}
     secondaryTypographyProps={{ noWrap: true }}
+    sx={{
+      overflow: "hidden",
+      textOverflow: "ellipsis",
+    }}
     {...props}
   />
 );
