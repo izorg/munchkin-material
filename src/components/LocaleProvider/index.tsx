bd1472127022776c99dddb84db3b8e28ccf138f2
Viacheslav Zavoruev <izorgster@gmail.com>
2022-06-05 11:36:54 +0200

    Use startTransition for locale loading

--- src/components/LocaleProvider/index.tsx
+++ src/components/LocaleProvider/index.tsx
@@ -1,92 +1,49 @@
 import { captureException } from "@sentry/react";
 import PropTypes from "prop-types";
-import { type FC, type PropsWithChildren, useEffect, useState } from "react";
-import { type IntlConfig, IntlProvider } from "react-intl";
+import {
+  type FC,
+  type PropsWithChildren,
+  useEffect,
+  useLayoutEffect,
+  useState,
+  useTransition,
+} from "react";
+import { IntlProvider } from "react-intl";
 
-import { getDirection, getLocale, loadMessages } from "../../i18n";
-import store from "../../store";
-import AsyncResource from "../../utils/AsyncResource";
+import { getDirection, getLocale } from "../../i18n";
 import usePresentSelector from "../../utils/usePresentSelector";
 
-import polyfillIntl from "./polyfillIntl";
+import readMessages from "./readMessages";
 
 const defaultLocale = getLocale();
 
-type LocalState = {
-  locale: string;
-} & (
-  | {
-      error: Error;
-    }
-  | {
-      messages: IntlConfig["messages"];
-    }
-);
-
-const initialLocale = store.getState().present.settings.locale || defaultLocale;
-
-const polyfillResource = new AsyncResource(polyfillIntl(initialLocale));
-
-const messagesResource = new AsyncResource(loadMessages(initialLocale));
-
 const LocaleProvider: FC<PropsWithChildren<unknown>> = ({ children }) => {
-  polyfillResource.read();
-
-  const locale =
+  const selectedLocale =
     usePresentSelector((state) => state.settings.locale) || defaultLocale;
 
-  const [localeState, setLocaleState] = useState<LocalState>({
-    locale,
-    messages: messagesResource.read(),
-  });
+  const [locale, setLocale] = useState(selectedLocale);
+
+  const messages = readMessages(locale);
+
+  const [, startTransition] = useTransition();
 
   useEffect(() => {
-    let active = true;
+    if (selectedLocale !== locale) {
+      startTransition(() => {
+        setLocale(selectedLocale);
+      });
+    }
+  }, [locale, selectedLocale]);
 
-    void (async () => {
-      await polyfillIntl(locale);
-
-      let messages;
-
-      try {
-        messages = await loadMessages(locale);
-      } catch (error) {
-        if (active && error instanceof Error) {
-          setLocaleState({
-            error,
-            locale,
-          });
-        }
-
-        return;
-      }
-
-      if (active) {
-        setLocaleState({
-          locale,
-          messages,
-        });
-      }
-    })();
-
-    return () => {
-      active = false;
-    };
+  useLayoutEffect(() => {
+    document.documentElement.lang = locale;
+    document.body.dir = getDirection(locale);
   }, [locale]);
-
-  useEffect(() => {
-    document.documentElement.lang = localeState.locale;
-    document.body.dir = getDirection(localeState.locale);
-  }, [localeState]);
-
-  if ("error" in localeState) {
-    throw localeState.error;
-  }
 
   return (
     <IntlProvider
-      locale={localeState.locale}
-      messages={localeState.messages}
+      locale={locale}
+      messages={messages}
       onError={
         process.env.NODE_ENV === "production" ? captureException : undefined
       }
