0b4794a9d267b42c25b13b6f5a1b0b6ff1f60223
Viacheslav Zavoruev <izorgster@gmail.com>
2022-06-21 17:27:20 +0200

    Simplify Legends theme

--- src/theme/colors/legends.ts
+++ src/theme/colors/legends.ts
@@ -1,6 +1,5 @@
 import { type ThemeOptions } from "@mui/material";
-import { common, grey } from "@mui/material/colors";
-import { light } from "@mui/material/styles/createPalette";
+import { grey } from "@mui/material/colors";
 import { defineMessages } from "react-intl";
 
 export const key = "legends";
@@ -16,37 +15,10 @@ export const name = messages.name;
 
 export const theme: ThemeOptions = {
   components: {
-    MuiAppBar: {
-      styleOverrides: {
-        colorPrimary: {
-          backgroundColor: grey[200],
-          color: light.text.primary,
-        },
-      },
-    },
-
     MuiAvatar: {
       styleOverrides: {
         colorDefault: {
           backgroundColor: grey[500],
-        },
-      },
-    },
-
-    MuiButton: {
-      styleOverrides: {
-        containedPrimary: {
-          /* eslint-disable sort-keys */
-          color: common.black,
-          backgroundColor: grey[200],
-          "&:hover": {
-            backgroundColor: grey[400],
-            // Reset on touch devices, it doesn't add specificity
-            "@media (hover: none)": {
-              backgroundColor: grey[200],
-            },
-          },
-          /* eslint-enable */
         },
       },
     },
