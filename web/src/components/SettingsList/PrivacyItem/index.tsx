f55633cf71f04d2e29ccece8b62a3e41c3569943
Viacheslav Zavoruev <izorgster@gmail.com>
2023-03-09 19:14:08 +0100

    Remove cordova from PrivacyItem

--- web/src/components/SettingsList/PrivacyItem/index.tsx
+++ web/src/components/SettingsList/PrivacyItem/index.tsx
@@ -5,34 +5,28 @@ import { FormattedMessage } from "react-intl";
 import ListItem from "../Item";
 import ListItemText from "../ItemText";
 
-const PrivacyItem = () => {
-  if (!("cordova" in window)) {
-    return null;
-  }
-
-  return (
-    <ListItem
-      button
-      component="a"
-      href="https://allmunchkins.com/privacy"
-      rel="noopener noreferrer"
-      target="_blank"
-    >
-      <ListItemIcon>
-        <SvgIcon>
-          <path d={mdiShieldAccountOutline} />
-        </SvgIcon>
-      </ListItemIcon>
-      <ListItemText
-        primary={
-          <FormattedMessage
-            defaultMessage="Privacy Policy"
-            id="menu.privacyPolicy"
-          />
-        }
-      />
-    </ListItem>
-  );
-};
+const PrivacyItem = () => (
+  <ListItem
+    button
+    component="a"
+    href="https://allmunchkins.com/privacy"
+    rel="noopener noreferrer"
+    target="_blank"
+  >
+    <ListItemIcon>
+      <SvgIcon>
+        <path d={mdiShieldAccountOutline} />
+      </SvgIcon>
+    </ListItemIcon>
+    <ListItemText
+      primary={
+        <FormattedMessage
+          defaultMessage="Privacy Policy"
+          id="menu.privacyPolicy"
+        />
+      }
+    />
+  </ListItem>
+);
 
 export default PrivacyItem;
