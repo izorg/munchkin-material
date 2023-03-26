5684a8d1814efc6393b390bfe3c020f73eddc100
Viacheslav Zavoruev <izorgster@gmail.com>
2023-03-27 00:14:23 +0200

    Try url_prefix in actions instead of RewriteFrames integration

--- cordova/src/sentry.ts
+++ cordova/src/sentry.ts
@@ -1,4 +1,3 @@
-import { RewriteFrames } from "@sentry/integrations";
 import {
   init,
   makeBrowserOfflineTransport,
@@ -9,7 +8,6 @@ init({
   dsn: "https://2f751739a828470aaab26658058e3fe5@o115150.ingest.sentry.io/5449939",
   enabled: !window.BuildInfo.debug,
   environment: process.env.NODE_ENV,
-  integrations: [new RewriteFrames()],
   normalizeDepth: 10,
   release: window.BuildInfo.version,
   transport: makeBrowserOfflineTransport(makeFetchTransport),
