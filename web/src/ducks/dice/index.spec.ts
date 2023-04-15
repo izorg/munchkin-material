6934577628b92d1aa59b7e1435a4fdc8d1626d6c
Viacheslav Zavoruev <izorgster@gmail.com>
2023-04-16 01:20:55 +0200

    Fix dice test

--- web/src/ducks/dice/index.spec.ts
+++ web/src/ducks/dice/index.spec.ts
@@ -3,11 +3,12 @@ import { describe, expect, test } from "@jest/globals";
 import reducer, { initialState, throwDice } from "./index";
 
 describe("Dice reducer", () => {
-  test("should set dice value from action", () => {
+  test("should set random dice value", () => {
     const action = throwDice();
 
     const dice = reducer(initialState, action);
 
-    expect(dice).toBe(action.payload);
+    expect(dice).toBeGreaterThanOrEqual(1);
+    expect(dice).toBeLessThanOrEqual(6);
   });
 });
