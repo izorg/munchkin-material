adb853078ff0e1fda3ea07ed81a1c4cda4283cbf
Viacheslav Zavoruev <izorgster@gmail.com>
2023-04-15 23:47:32 +0200

    Extract getRandomDice

--- web/src/ducks/dice/index.ts
+++ web/src/ducks/dice/index.ts
@@ -2,15 +2,18 @@ import { createSlice, type PayloadAction } from "@reduxjs/toolkit";
 
 export type DiceValue = 1 | 2 | 3 | 4 | 5 | 6;
 
-export const initialState = null;
+// Use `as` to correct state type https://redux-toolkit.js.org/tutorials/typescript#define-slice-state-and-action-types
+export const initialState = null as DiceValue | null;
+
+const getRandomDice = () => (Math.floor(Math.random() * 6) + 1) as DiceValue;
 
 export const diceSlice = createSlice({
-  initialState: initialState as DiceValue | null,
+  initialState,
   name: "dice",
   reducers: {
     throwDice: {
       prepare: () => ({
-        payload: (Math.floor(Math.random() * 6) + 1) as DiceValue,
+        payload: getRandomDice(),
       }),
       reducer: (state, action: PayloadAction<DiceValue>) => action.payload,
     },
