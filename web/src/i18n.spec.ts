369390bc8ba1934a6eca14804eae4196587c0b00
Viacheslav Zavoruev <izorgster@gmail.com>
2023-04-16 12:39:30 +0200

    Use spyOn for `navigator.languages` mock

--- web/src/i18n.spec.ts
+++ web/src/i18n.spec.ts
@@ -2,62 +2,49 @@
  * @jest-environment jsdom
  */
 
-import { describe, expect, test } from "@jest/globals";
+import { afterEach, describe, expect, jest, test } from "@jest/globals";
 
 import { getLocale } from "./i18n";
 
 describe("getLocale()", () => {
+  afterEach(() => {
+    jest.resetAllMocks();
+  });
+
   test("should get ru locale", () => {
-    Object.defineProperty(window.navigator, "languages", {
-      configurable: true,
-      get() {
-        return ["ru"];
-      },
-    });
+    jest.spyOn(navigator, "languages", "get").mockImplementation(() => ["ru"]);
 
     expect(getLocale()).toBe("ru");
   });
 
   test("should get ru locale from ru-RU language", () => {
-    Object.defineProperty(window.navigator, "languages", {
-      configurable: true,
-      get() {
-        return ["ru-RU"];
-      },
-    });
+    jest
+      .spyOn(navigator, "languages", "get")
+      .mockImplementation(() => ["ru-RU"]);
 
     expect(getLocale()).toBe("ru");
   });
 
   test("should get pt locale from pt-PT language", () => {
-    Object.defineProperty(window.navigator, "languages", {
-      configurable: true,
-      get() {
-        return ["pt-PT", "pt-BR"];
-      },
-    });
+    jest
+      .spyOn(navigator, "languages", "get")
+      .mockImplementation(() => ["pt-PT", "pt-BR"]);
 
     expect(getLocale()).toBe("pt");
   });
 
   test("should get pt-BR locale", () => {
-    Object.defineProperty(window.navigator, "languages", {
-      configurable: true,
-      get() {
-        return ["pt-BR", "pt"];
-      },
-    });
+    jest
+      .spyOn(navigator, "languages", "get")
+      .mockImplementation(() => ["pt-BR", "pt"]);
 
     expect(getLocale()).toBe("pt-BR");
   });
 
   test("should get en locale from en-US languages", () => {
-    Object.defineProperty(window.navigator, "languages", {
-      configurable: true,
-      get() {
-        return ["en-US"];
-      },
-    });
+    jest
+      .spyOn(navigator, "languages", "get")
+      .mockImplementation(() => ["en-US"]);
 
     expect(getLocale()).toBe("en");
   });
