f48c31f3227a5e8858fa2d57025effe8d08292be
Viacheslav Zavoruev <izorgster@gmail.com>
2023-06-04 14:22:45 +0200

    Enable unicorn/prefer-dom-node-append

--- cordova/src/index.ts
+++ cordova/src/index.ts
@@ -4,4 +4,4 @@ document.addEventListener("deviceready", () => import("./cordova"), false);
 
 const cordovaScript = document.createElement("script");
 cordovaScript.setAttribute("src", "cordova.js");
-document.querySelector("head")?.appendChild(cordovaScript);
+document.querySelector("head")?.append(cordovaScript);
