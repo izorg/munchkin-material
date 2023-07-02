c7a4636819dbeceef88713877b9a56a35cf8e8dd
Viacheslav Zavoruev <izorgster@gmail.com>
2023-07-02 18:10:39 +0200

    Enable unicorn/prefer-top-level-await

--- scripts/i18n/download.js
+++ scripts/i18n/download.js
@@ -19,31 +19,29 @@ const writeTranslation = async (code, data) => {
   console.log(`✅ ${code.toUpperCase()}`);
 };
 
-(async () => {
-  console.log("Downloading locales:");
+console.log("Downloading locales:");
 
-  const response = await poeditor.post("/languages/list", {
-    id: projectId,
-  });
+const response = await poeditor.post("/languages/list", {
+  id: projectId,
+});
 
-  const languages = response.data.result.languages
-    .map(({ code }) => code)
-    .filter((code) => code !== "en");
+const languages = response.data.result.languages
+  .map(({ code }) => code)
+  .filter((code) => code !== "en");
 
-  await Promise.all(
-    languages.map(async (language) => {
-      const url = await poeditor
-        .post("/projects/export", {
-          id: projectId,
-          language,
-          order: "terms",
-          type: "key_value_json",
-        })
-        .then(({ data }) => data.result.url);
+await Promise.all(
+  languages.map(async (language) => {
+    const url = await poeditor
+      .post("/projects/export", {
+        id: projectId,
+        language,
+        order: "terms",
+        type: "key_value_json",
+      })
+      .then(({ data }) => data.result.url);
 
-      const content = await poeditor.get(url).then(({ data }) => data);
+    const content = await poeditor.get(url).then(({ data }) => data);
 
-      await writeTranslation(language.toLowerCase(), content);
-    })
-  );
-})();
+    await writeTranslation(language.toLowerCase(), content);
+  })
+);
