68dc8a5661bdf7bbf34c1d468246a0f8f5151da6
Viacheslav Zavoruev <izorgster@gmail.com>
2023-12-06 22:39:04 +0100

    Fix Sonar random number issue

--- packages/web/src/ducks/dice/index.ts
+++ packages/web/src/ducks/dice/index.ts
@@ -1,11 +1,13 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-export type DiceValue = 1 | 2 | 3 | 4 | 5 | 6;
+import { getRandomInt } from "../../utils/getRandomInt";
+
+type DiceValue = 1 | 2 | 3 | 4 | 5 | 6;
 
 // Use `as` to correct state type https://redux-toolkit.js.org/tutorials/typescript#define-slice-state-and-action-types
 export const initialState = null as DiceValue | null;
 
-const getRandomDice = () => (Math.floor(Math.random() * 6) + 1) as DiceValue;
+const getRandomDice = () => getRandomInt(1, 6) as DiceValue;
 
 const diceSlice = createSlice({
   initialState,
---
+++ packages/web/src/utils/getRandomInt.ts
@@ -0,0 +1,12 @@
+export const getRandomInt = (min: number, max: number) => {
+  const randomBuffer = new Uint32Array(1);
+
+  window.crypto.getRandomValues(randomBuffer);
+
+  const randomNumber = randomBuffer[0] / (0xff_ff_ff_ff + 1);
+
+  min = Math.ceil(min);
+  max = Math.floor(max);
+
+  return Math.floor(randomNumber * (max - min + 1)) + min;
+};
