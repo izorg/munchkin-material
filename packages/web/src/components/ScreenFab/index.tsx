76350d5bac8954269e26760493e50162322f638f
Viacheslav Zavoruev <izorgster@gmail.com>
2023-12-19 20:58:56 +0100

    ScreenFab RTL support

--- packages/web/src/components/ScreenFab/index.tsx
+++ packages/web/src/components/ScreenFab/index.tsx
@@ -1,36 +1,57 @@
-import { Fab, type FabProps, useTheme } from "@mui/material";
+import { Fab, type FabProps } from "@mui/material";
 import { forwardRef } from "react";
 
 const ScreenFab = forwardRef<HTMLButtonElement, FabProps>(
   function ScreenFab(props, ref) {
-    const theme = useTheme();
+    const { sx = [], ...rest } = props;
 
     return (
       <Fab
         ref={ref}
         color="primary"
         sx={[
-          {
+          (theme) => ({
             bottom: {
               sm: theme.spacing(3),
               xs: theme.spacing(2),
             },
+            left: "auto",
             position: "fixed",
             right: {
               sm: theme.spacing(3),
               xs: theme.spacing(2),
             },
             zIndex: 2,
-          },
-          {
+          }),
+          (theme) => ({
             "@supports (padding: max(0px))": {
-              right: {
-                sm: `max(${theme.spacing(3)}, env(safe-area-inset-right))`,
+              '[dir="ltr"] &': {
+                left: "auto /*! @noflip */",
+                right: {
+                  sm: `max(${theme.spacing(
+                    3,
+                  )}, env(safe-area-inset-right)) /*! @noflip */`,
+                  xs: `max(${theme.spacing(
+                    2,
+                  )}, env(safe-area-inset-right)) /*! @noflip */`,
+                },
+              },
+              '[dir="rtl"] &': {
+                left: {
+                  sm: `max(${theme.spacing(
+                    3,
+                  )}, env(safe-area-inset-left)) /*! @noflip */`,
+                  xs: `max(${theme.spacing(
+                    2,
+                  )}, env(safe-area-inset-left)) /*! @noflip */`,
+                },
+                right: "auto /*! @noflip */",
               },
             },
-          },
+          }),
+          ...(sx instanceof Array ? sx : [sx]),
         ]}
-        {...props}
+        {...rest}
       />
     );
   },
