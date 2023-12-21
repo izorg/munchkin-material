87c10cf54852050714fd5356ca401b111f711978
Viacheslav Zavoruev <izorgster@gmail.com>
2023-12-21 22:43:51 +0100

    Use theme.spacing [skip ci]

--- packages/web/src/components/SettingsList/SettingsListItemButton.tsx
+++ packages/web/src/components/SettingsList/SettingsListItemButton.tsx
@@ -1,8 +1,8 @@
 import { ListItemButton, styled } from "@mui/material";
 
-export const SettingsListItemButton = styled(ListItemButton)(() => ({
+export const SettingsListItemButton = styled(ListItemButton)(({ theme }) => ({
   "@supports (padding: max(0px)) and (padding: env(safe-area-inset-left))": {
-    paddingLeft: "max(16px, env(safe-area-inset-left))",
-    paddingRight: "max(16px, env(safe-area-inset-left))",
+    paddingLeft: `max(${theme.spacing(2)}, env(safe-area-inset-left))`,
+    paddingRight: `max(${theme.spacing(2)}, env(safe-area-inset-left))`,
   },
 })) as typeof ListItemButton;
