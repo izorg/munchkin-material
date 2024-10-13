0b1c0fbed0a55da9708b57a7c691eca2f786dfe4
Viacheslav Zavoruev <izorgster@gmail.com>
2024-10-13 11:48:45 +0200

    chore: use responsive font on site

--- packages/site/lib/theme.ts
+++ packages/site/lib/theme.ts
@@ -1,39 +1,41 @@
 "use client";
 
-import { createTheme } from "@mui/material";
+import { createTheme, responsiveFontSizes } from "@mui/material";
 import brown from "@mui/material/colors/brown";
 
-export const theme = createTheme({
-  colorSchemes: {
-    dark: {
-      palette: {
-        primary: {
-          main: brown[200],
+export const theme = responsiveFontSizes(
+  createTheme({
+    colorSchemes: {
+      dark: {
+        palette: {
+          primary: {
+            main: brown[200],
+          },
+        },
+      },
+      light: {
+        palette: {
+          primary: {
+            main: brown[500],
+          },
         },
       },
     },
-    light: {
-      palette: {
-        primary: {
-          main: brown[500],
+
+    components: {
+      MuiButton: {
+        styleOverrides: {
+          root: {
+            textTransform: "none",
+          },
         },
       },
     },
-  },
 
-  components: {
-    MuiButton: {
-      styleOverrides: {
-        root: {
-          textTransform: "none",
-        },
-      },
+    cssVariables: true,
+
+    typography: {
+      fontFamily: '"Roboto", "San Francisco", "Helvetica", "Arial", sans-serif',
     },
-  },
-
-  cssVariables: true,
-
-  typography: {
-    fontFamily: '"Roboto", "San Francisco", "Helvetica", "Arial", sans-serif',
-  },
-});
+  }),
+);
