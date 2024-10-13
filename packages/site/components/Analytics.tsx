09c8f7459dc37d6a8f15dca2bd4d089408bb9da7
Viacheslav Zavoruev <izorgster@gmail.com>
2024-10-13 17:09:12 +0200

    chore: setup Google Analytics

--- packages/site/components/Analytics.tsx
+++
@@ -1,3 +0,0 @@
-import { GoogleTagManager } from "@next/third-parties/google";
-
-export const Analytics = () => <GoogleTagManager gtmId="GTM-5Z3D8FX" />;
--- packages/site/components/RootLayout.tsx
+++ packages/site/components/RootLayout.tsx
@@ -1,10 +1,10 @@
+import { GoogleAnalytics } from "@next/third-parties/google";
 import { type Viewport } from "next";
 import { type PropsWithChildren } from "react";
 
 import { getLocalMessages, type Language, localeByLanguage } from "../lib/i18n";
 import { munchkinFont } from "../lib/munchkinFont";
 
-import { Analytics } from "./Analytics";
 import { I18nProvider } from "./I18nProvider";
 import { ThemeRegistry } from "./ThemeRegistry";
 
@@ -22,12 +22,12 @@ export const RootLayout = async ({ children, language }: RootLayoutProps) => {
 
   return (
     <html className={munchkinFont.variable} lang={locale}>
+      <GoogleAnalytics gaId="G-3HCBBLXXS0" />
       <body>
         <script>window.globalThis = window;</script>
         <I18nProvider locale={locale} messages={messages}>
           <ThemeRegistry>{children}</ThemeRegistry>
         </I18nProvider>
-        <Analytics />
       </body>
     </html>
   );
