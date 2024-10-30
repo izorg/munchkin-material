7515f3de69ed49a0e4b74e26e43b484c0215b4ff
Viacheslav Zavoruev <izorgster@gmail.com>
2024-10-30 21:26:29 +0100

    chore: update configs

--- packages/site/app/manifest.ts
+++ packages/site/app/manifest.ts
@@ -3,6 +3,8 @@ import { type MetadataRoute } from "next";
 import icon192 from "./icon-192.png";
 import icon512 from "./icon-512.png";
 
+export const dynamic = "force-static";
+
 const manifest = (): MetadataRoute.Manifest => ({
   icons: [
     {
