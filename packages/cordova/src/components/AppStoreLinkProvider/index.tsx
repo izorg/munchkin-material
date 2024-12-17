914cc89b07aae04ca7e2c0a01c2d0e40432f6e7d
Viacheslav Zavoruev <izorgster@gmail.com>
2024-12-17 08:45:41 +0100

    chore: use map instead of function for app store link

--- packages/cordova/src/components/AppStoreLinkProvider/index.tsx
+++ packages/cordova/src/components/AppStoreLinkProvider/index.tsx
@@ -1,27 +1,14 @@
-import { type FC, type PropsWithChildren, useMemo } from "react";
+import { type FC, type PropsWithChildren } from "react";
 
 import { AppStoreLinkContext } from "../../../../web/src/utils/appStoreLinkContext";
 
-const getAppStoreLink = () => {
-  const { platformId } = window.cordova;
-
-  switch (platformId) {
-    case "android": {
-      return "market://details?id=com.izorg.munchkin";
-    }
-
-    case "ios": {
-      return "itms-apps://itunes.apple.com/WebObjects/MZStore.woa/wa/viewContentsUserReviews?type=Purple+Software&id=1448937097";
-    }
-
-    default: {
-      throw new Error(`Unknown platform "${platformId}"`);
-    }
-  }
+const appStoreLink: Partial<Record<string, string>> = {
+  android: "market://details?id=com.izorg.munchkin",
+  ios: "itms-apps://itunes.apple.com/WebObjects/MZStore.woa/wa/viewContentsUserReviews?type=Purple+Software&id=1448937097",
 };
 
 const AppStoreLinkProvider: FC<PropsWithChildren> = ({ children }) => (
-  <AppStoreLinkContext.Provider value={useMemo(getAppStoreLink, [])}>
+  <AppStoreLinkContext.Provider value={appStoreLink[window.cordova.platformId]}>
     {children}
   </AppStoreLinkContext.Provider>
 );
