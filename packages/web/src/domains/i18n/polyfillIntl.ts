cf0f6193d3d7973b53b87b73ab1becf14c23806e
Viacheslav Zavoruev <izorgster@gmail.com>
2025-12-17 21:58:06 +0100

    chore: packages upgrade

--- packages/web/src/domains/i18n/polyfillIntl.ts
+++ packages/web/src/domains/i18n/polyfillIntl.ts
@@ -1,8 +1,8 @@
-import { shouldPolyfill as shouldPolyfillDateTimeFormat } from "@formatjs/intl-datetimeformat/should-polyfill";
-import { shouldPolyfill as shouldPolyfillGetCanonicalLocales } from "@formatjs/intl-getcanonicallocales/should-polyfill";
-import { shouldPolyfill as shouldPolyfillIntlLocale } from "@formatjs/intl-locale/should-polyfill";
-import { shouldPolyfill as shouldPolyfillNumberFormat } from "@formatjs/intl-numberformat/should-polyfill";
-import { shouldPolyfill as shouldPolyfillPluralRules } from "@formatjs/intl-pluralrules/should-polyfill";
+import { shouldPolyfill as shouldPolyfillDateTimeFormat } from "@formatjs/intl-datetimeformat/should-polyfill.js";
+import { shouldPolyfill as shouldPolyfillGetCanonicalLocales } from "@formatjs/intl-getcanonicallocales/should-polyfill.js";
+import { shouldPolyfill as shouldPolyfillIntlLocale } from "@formatjs/intl-locale/should-polyfill.js";
+import { shouldPolyfill as shouldPolyfillNumberFormat } from "@formatjs/intl-numberformat/should-polyfill.js";
+import { shouldPolyfill as shouldPolyfillPluralRules } from "@formatjs/intl-pluralrules/should-polyfill.js";
 
 import {
   BE,
@@ -31,94 +31,94 @@ import {
 } from "./supportedLocales";
 
 const pluralRulesLoaders: Record<SupportedLocale, () => unknown> = {
-  [BE]: () => import("@formatjs/intl-pluralrules/locale-data/be"),
-  [CS]: () => import("@formatjs/intl-pluralrules/locale-data/cs"),
-  [DA]: () => import("@formatjs/intl-pluralrules/locale-data/da"),
-  [DE]: () => import("@formatjs/intl-pluralrules/locale-data/de"),
-  [EL]: () => import("@formatjs/intl-pluralrules/locale-data/el"),
-  [EN]: () => import("@formatjs/intl-pluralrules/locale-data/en"),
-  [ES]: () => import("@formatjs/intl-pluralrules/locale-data/es"),
-  [FI]: () => import("@formatjs/intl-pluralrules/locale-data/fi"),
-  [FR]: () => import("@formatjs/intl-pluralrules/locale-data/fr"),
-  [HE]: () => import("@formatjs/intl-pluralrules/locale-data/he"),
-  [HU]: () => import("@formatjs/intl-pluralrules/locale-data/hu"),
-  [HY]: () => import("@formatjs/intl-pluralrules/locale-data/hy"),
-  [IT]: () => import("@formatjs/intl-pluralrules/locale-data/it"),
-  [NB]: () => import("@formatjs/intl-pluralrules/locale-data/nb"),
-  [NL]: () => import("@formatjs/intl-pluralrules/locale-data/nl"),
-  [PL]: () => import("@formatjs/intl-pluralrules/locale-data/pl"),
-  [PT]: () => import("@formatjs/intl-pluralrules/locale-data/pt"),
-  [PT_BR]: () => import("@formatjs/intl-pluralrules/locale-data/pt"),
-  [RU]: () => import("@formatjs/intl-pluralrules/locale-data/ru"),
-  [SK]: () => import("@formatjs/intl-pluralrules/locale-data/sk"),
-  [TR]: () => import("@formatjs/intl-pluralrules/locale-data/tr"),
-  [UK]: () => import("@formatjs/intl-pluralrules/locale-data/uk"),
+  [BE]: () => import("@formatjs/intl-pluralrules/locale-data/be.js"),
+  [CS]: () => import("@formatjs/intl-pluralrules/locale-data/cs.js"),
+  [DA]: () => import("@formatjs/intl-pluralrules/locale-data/da.js"),
+  [DE]: () => import("@formatjs/intl-pluralrules/locale-data/de.js"),
+  [EL]: () => import("@formatjs/intl-pluralrules/locale-data/el.js"),
+  [EN]: () => import("@formatjs/intl-pluralrules/locale-data/en.js"),
+  [ES]: () => import("@formatjs/intl-pluralrules/locale-data/es.js"),
+  [FI]: () => import("@formatjs/intl-pluralrules/locale-data/fi.js"),
+  [FR]: () => import("@formatjs/intl-pluralrules/locale-data/fr.js"),
+  [HE]: () => import("@formatjs/intl-pluralrules/locale-data/he.js"),
+  [HU]: () => import("@formatjs/intl-pluralrules/locale-data/hu.js"),
+  [HY]: () => import("@formatjs/intl-pluralrules/locale-data/hy.js"),
+  [IT]: () => import("@formatjs/intl-pluralrules/locale-data/it.js"),
+  [NB]: () => import("@formatjs/intl-pluralrules/locale-data/nb.js"),
+  [NL]: () => import("@formatjs/intl-pluralrules/locale-data/nl.js"),
+  [PL]: () => import("@formatjs/intl-pluralrules/locale-data/pl.js"),
+  [PT]: () => import("@formatjs/intl-pluralrules/locale-data/pt.js"),
+  [PT_BR]: () => import("@formatjs/intl-pluralrules/locale-data/pt.js"),
+  [RU]: () => import("@formatjs/intl-pluralrules/locale-data/ru.js"),
+  [SK]: () => import("@formatjs/intl-pluralrules/locale-data/sk.js"),
+  [TR]: () => import("@formatjs/intl-pluralrules/locale-data/tr.js"),
+  [UK]: () => import("@formatjs/intl-pluralrules/locale-data/uk.js"),
 };
 
 const numberFormatLoaders: Record<SupportedLocale, () => unknown> = {
-  [BE]: () => import("@formatjs/intl-numberformat/locale-data/be"),
-  [CS]: () => import("@formatjs/intl-numberformat/locale-data/cs"),
-  [DA]: () => import("@formatjs/intl-numberformat/locale-data/da"),
-  [DE]: () => import("@formatjs/intl-numberformat/locale-data/de"),
-  [EL]: () => import("@formatjs/intl-numberformat/locale-data/el"),
-  [EN]: () => import("@formatjs/intl-numberformat/locale-data/en"),
-  [ES]: () => import("@formatjs/intl-numberformat/locale-data/es"),
-  [FI]: () => import("@formatjs/intl-numberformat/locale-data/fi"),
-  [FR]: () => import("@formatjs/intl-numberformat/locale-data/fr"),
-  [HE]: () => import("@formatjs/intl-numberformat/locale-data/he"),
-  [HU]: () => import("@formatjs/intl-numberformat/locale-data/hu"),
-  [HY]: () => import("@formatjs/intl-numberformat/locale-data/hy"),
-  [IT]: () => import("@formatjs/intl-numberformat/locale-data/it"),
-  [NB]: () => import("@formatjs/intl-numberformat/locale-data/nb"),
-  [NL]: () => import("@formatjs/intl-numberformat/locale-data/nl"),
-  [PL]: () => import("@formatjs/intl-numberformat/locale-data/pl"),
-  [PT]: () => import("@formatjs/intl-numberformat/locale-data/pt"),
-  [PT_BR]: () => import("@formatjs/intl-numberformat/locale-data/pt"),
-  [RU]: () => import("@formatjs/intl-numberformat/locale-data/ru"),
-  [SK]: () => import("@formatjs/intl-numberformat/locale-data/sk"),
-  [TR]: () => import("@formatjs/intl-numberformat/locale-data/tr"),
-  [UK]: () => import("@formatjs/intl-numberformat/locale-data/uk"),
+  [BE]: () => import("@formatjs/intl-numberformat/locale-data/be.js"),
+  [CS]: () => import("@formatjs/intl-numberformat/locale-data/cs.js"),
+  [DA]: () => import("@formatjs/intl-numberformat/locale-data/da.js"),
+  [DE]: () => import("@formatjs/intl-numberformat/locale-data/de.js"),
+  [EL]: () => import("@formatjs/intl-numberformat/locale-data/el.js"),
+  [EN]: () => import("@formatjs/intl-numberformat/locale-data/en.js"),
+  [ES]: () => import("@formatjs/intl-numberformat/locale-data/es.js"),
+  [FI]: () => import("@formatjs/intl-numberformat/locale-data/fi.js"),
+  [FR]: () => import("@formatjs/intl-numberformat/locale-data/fr.js"),
+  [HE]: () => import("@formatjs/intl-numberformat/locale-data/he.js"),
+  [HU]: () => import("@formatjs/intl-numberformat/locale-data/hu.js"),
+  [HY]: () => import("@formatjs/intl-numberformat/locale-data/hy.js"),
+  [IT]: () => import("@formatjs/intl-numberformat/locale-data/it.js"),
+  [NB]: () => import("@formatjs/intl-numberformat/locale-data/nb.js"),
+  [NL]: () => import("@formatjs/intl-numberformat/locale-data/nl.js"),
+  [PL]: () => import("@formatjs/intl-numberformat/locale-data/pl.js"),
+  [PT]: () => import("@formatjs/intl-numberformat/locale-data/pt.js"),
+  [PT_BR]: () => import("@formatjs/intl-numberformat/locale-data/pt.js"),
+  [RU]: () => import("@formatjs/intl-numberformat/locale-data/ru.js"),
+  [SK]: () => import("@formatjs/intl-numberformat/locale-data/sk.js"),
+  [TR]: () => import("@formatjs/intl-numberformat/locale-data/tr.js"),
+  [UK]: () => import("@formatjs/intl-numberformat/locale-data/uk.js"),
 };
 
 const dateTimeFormatLoaders: Record<SupportedLocale, () => unknown> = {
-  [BE]: () => import("@formatjs/intl-datetimeformat/locale-data/be"),
-  [CS]: () => import("@formatjs/intl-datetimeformat/locale-data/cs"),
-  [DA]: () => import("@formatjs/intl-datetimeformat/locale-data/da"),
-  [DE]: () => import("@formatjs/intl-datetimeformat/locale-data/de"),
-  [EL]: () => import("@formatjs/intl-datetimeformat/locale-data/el"),
-  [EN]: () => import("@formatjs/intl-datetimeformat/locale-data/en"),
-  [ES]: () => import("@formatjs/intl-datetimeformat/locale-data/es"),
-  [FI]: () => import("@formatjs/intl-datetimeformat/locale-data/fi"),
-  [FR]: () => import("@formatjs/intl-datetimeformat/locale-data/fr"),
-  [HE]: () => import("@formatjs/intl-datetimeformat/locale-data/he"),
-  [HU]: () => import("@formatjs/intl-datetimeformat/locale-data/hu"),
-  [HY]: () => import("@formatjs/intl-datetimeformat/locale-data/hy"),
-  [IT]: () => import("@formatjs/intl-datetimeformat/locale-data/it"),
-  [NB]: () => import("@formatjs/intl-datetimeformat/locale-data/nb"),
-  [NL]: () => import("@formatjs/intl-datetimeformat/locale-data/nl"),
-  [PL]: () => import("@formatjs/intl-datetimeformat/locale-data/pl"),
-  [PT]: () => import("@formatjs/intl-datetimeformat/locale-data/pt"),
-  [PT_BR]: () => import("@formatjs/intl-datetimeformat/locale-data/pt"),
-  [RU]: () => import("@formatjs/intl-datetimeformat/locale-data/ru"),
-  [SK]: () => import("@formatjs/intl-datetimeformat/locale-data/sk"),
-  [TR]: () => import("@formatjs/intl-datetimeformat/locale-data/tr"),
-  [UK]: () => import("@formatjs/intl-datetimeformat/locale-data/uk"),
+  [BE]: () => import("@formatjs/intl-datetimeformat/locale-data/be.js"),
+  [CS]: () => import("@formatjs/intl-datetimeformat/locale-data/cs.js"),
+  [DA]: () => import("@formatjs/intl-datetimeformat/locale-data/da.js"),
+  [DE]: () => import("@formatjs/intl-datetimeformat/locale-data/de.js"),
+  [EL]: () => import("@formatjs/intl-datetimeformat/locale-data/el.js"),
+  [EN]: () => import("@formatjs/intl-datetimeformat/locale-data/en.js"),
+  [ES]: () => import("@formatjs/intl-datetimeformat/locale-data/es.js"),
+  [FI]: () => import("@formatjs/intl-datetimeformat/locale-data/fi.js"),
+  [FR]: () => import("@formatjs/intl-datetimeformat/locale-data/fr.js"),
+  [HE]: () => import("@formatjs/intl-datetimeformat/locale-data/he.js"),
+  [HU]: () => import("@formatjs/intl-datetimeformat/locale-data/hu.js"),
+  [HY]: () => import("@formatjs/intl-datetimeformat/locale-data/hy.js"),
+  [IT]: () => import("@formatjs/intl-datetimeformat/locale-data/it.js"),
+  [NB]: () => import("@formatjs/intl-datetimeformat/locale-data/nb.js"),
+  [NL]: () => import("@formatjs/intl-datetimeformat/locale-data/nl.js"),
+  [PL]: () => import("@formatjs/intl-datetimeformat/locale-data/pl.js"),
+  [PT]: () => import("@formatjs/intl-datetimeformat/locale-data/pt.js"),
+  [PT_BR]: () => import("@formatjs/intl-datetimeformat/locale-data/pt.js"),
+  [RU]: () => import("@formatjs/intl-datetimeformat/locale-data/ru.js"),
+  [SK]: () => import("@formatjs/intl-datetimeformat/locale-data/sk.js"),
+  [TR]: () => import("@formatjs/intl-datetimeformat/locale-data/tr.js"),
+  [UK]: () => import("@formatjs/intl-datetimeformat/locale-data/uk.js"),
 };
 
 export const polyfillIntlGetCanonicalLocales = async () => {
   if (shouldPolyfillGetCanonicalLocales()) {
-    await import("@formatjs/intl-getcanonicallocales/polyfill");
+    await import("@formatjs/intl-getcanonicallocales/polyfill.js");
   }
 };
 
 export const polyfillIntlLocale = async () => {
   if (shouldPolyfillIntlLocale()) {
-    await import("@formatjs/intl-locale/polyfill");
+    await import("@formatjs/intl-locale/polyfill.js");
   } else if (
     !("textInfo" in Intl.Locale.prototype) &&
     !("getTextInfo" in Intl.Locale.prototype)
   ) {
-    await import("@formatjs/intl-locale/polyfill-force");
+    await import("@formatjs/intl-locale/polyfill-force.js");
   }
 };
 
@@ -127,17 +127,17 @@ export const polyfillIntl = async (locale: SupportedLocale) => {
   await polyfillIntlLocale();
 
   if (shouldPolyfillPluralRules(locale)) {
-    await import("@formatjs/intl-pluralrules/polyfill-force");
+    await import("@formatjs/intl-pluralrules/polyfill-force.js");
     await pluralRulesLoaders[locale]();
   }
 
   if (shouldPolyfillNumberFormat(locale)) {
-    await import("@formatjs/intl-numberformat/polyfill-force");
+    await import("@formatjs/intl-numberformat/polyfill-force.js");
     await numberFormatLoaders[locale]();
   }
 
   if (shouldPolyfillDateTimeFormat(locale)) {
-    await import("@formatjs/intl-datetimeformat/polyfill-force");
+    await import("@formatjs/intl-datetimeformat/polyfill-force.js");
     await dateTimeFormatLoaders[locale]();
   }
 };
