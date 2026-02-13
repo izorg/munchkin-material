ba2a3ae624f2398accd57b3da0a8107789336593
Viacheslav Zavoruev <izorgster@gmail.com>
2026-02-13 23:58:05 +0100

    chore: move index from web to pwa

--- packages/pwa/src/index.tsx
+++ packages/pwa/src/index.tsx
@@ -1,24 +1,24 @@
-import "./polyfills";
-import "./sentry";
-import "./firebase";
+import "../../web/src/polyfills";
+import "../../web/src/sentry";
+import "../../web/src/firebase";
 
 import { reactErrorHandler } from "@sentry/react";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { HashRouter } from "react-router";
 
-import App from "./components/App";
-import AugmentedStylesProvider from "./components/AugmentedStylesProvider";
-import AugmentedThemeProvider from "./components/AugmentedThemeProvider";
-import LocaleProvider from "./components/LocaleProvider";
-import ReduxProvider from "./components/ReduxProvider";
-import VersionProvider from "./components/VersionProvider";
-import WakeLockProvider from "./components/WakeLockProvider";
-import WorkboxProvider from "./components/WorkboxProvider";
-import { WebApp } from "./domains/web-app";
+import App from "../../web/src/components/App";
+import AugmentedStylesProvider from "../../web/src/components/AugmentedStylesProvider";
+import AugmentedThemeProvider from "../../web/src/components/AugmentedThemeProvider";
+import LocaleProvider from "../../web/src/components/LocaleProvider";
+import ReduxProvider from "../../web/src/components/ReduxProvider";
+import VersionProvider from "../../web/src/components/VersionProvider";
+import WakeLockProvider from "../../web/src/components/WakeLockProvider";
+import WorkboxProvider from "../../web/src/components/WorkboxProvider";
+import { WebApp } from "../../web/src/domains/web-app";
 
 if (process.env.NODE_ENV === "development") {
-  void import("./dev");
+  void import("../../web/src/dev");
 }
 
 const node = document.querySelector("#root");
