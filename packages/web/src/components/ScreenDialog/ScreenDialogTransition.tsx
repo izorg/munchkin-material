30671d5753ea199b94d06e42392021a5343b7c70
Viacheslav Zavoruev <izorgster@gmail.com>
2026-03-15 20:45:54 +0100

    fix: fix `ScreenDialog` animation

--- packages/web/src/components/ScreenDialog/ScreenDialogTransition.tsx
+++ packages/web/src/components/ScreenDialog/ScreenDialogTransition.tsx
@@ -1,17 +1,31 @@
 import { Fade, Slide, useMediaQuery } from "@mui/material";
 import { type TransitionProps } from "@mui/material/transitions";
-import { type ReactElement } from "react";
+import { type ReactElement, useEffect } from "react";
 
 import { ios } from "../../utils/platforms";
 
-export const ScreenDialogTransition = (
-  props: { children: ReactElement } & Omit<TransitionProps, "children">,
-) => {
+let initialRender = true;
+
+type ScreenDialogTransitionProps = { children: ReactElement } & Omit<
+  TransitionProps,
+  "children"
+>;
+
+export const ScreenDialogTransition = ({
+  appear: appearProps,
+  ...rest
+}: ScreenDialogTransitionProps) => {
+  useEffect(() => {
+    initialRender = false;
+  }, []);
+
+  const appear = initialRender ? false : appearProps;
+
   const slide = useMediaQuery((theme) => theme.breakpoints.down("lg")) && ios;
 
   if (slide) {
-    return <Slide direction="left" {...props} />;
+    return <Slide appear={appear} direction="left" {...rest} />;
   }
 
-  return <Fade {...props} />;
+  return <Fade appear={appear} {...rest} />;
 };
--- packages/web/src/components/ScreenDialog/index.tsx
+++ packages/web/src/components/ScreenDialog/index.tsx
@@ -1,37 +1,19 @@
 import { Dialog, type DialogProps } from "@mui/material";
-import { useEffect } from "react";
 
 import { ScreenDialogPaper } from "./ScreenDialogPaper";
 import { ScreenDialogTransition } from "./ScreenDialogTransition";
 
-let initialRender = true;
-
-const ScreenDialog = (props: DialogProps) => {
-  const { slotProps, slots, ...rest } = props;
-
-  useEffect(() => {
-    initialRender = false;
-  }, []);
-
-  return (
-    <Dialog
-      fullScreen
-      hideBackdrop
-      PaperComponent={ScreenDialogPaper}
-      slotProps={{
-        ...slotProps,
-        transition: {
-          appear: !initialRender,
-          ...slotProps?.transition,
-        },
-      }}
-      slots={{
-        transition: ScreenDialogTransition,
-        ...slots,
-      }}
-      {...rest}
-    />
-  );
-};
+const ScreenDialog = ({ slots, ...rest }: DialogProps) => (
+  <Dialog
+    fullScreen
+    hideBackdrop
+    PaperComponent={ScreenDialogPaper}
+    slots={{
+      transition: ScreenDialogTransition,
+      ...slots,
+    }}
+    {...rest}
+  />
+);
 
 export default ScreenDialog;
