b1a7a0e4e61a41229d4b89d0c52785072841d735
Viacheslav Zavoruev <izorgster@gmail.com>
2026-04-06 18:09:05 +0200

    chore(cordova): upgrade cordova-android to v15

--- packages/cordova/src/domains/cordova-app/Insets.tsx
+++ packages/cordova/src/domains/cordova-app/Insets.tsx
@@ -1,41 +1,20 @@
 import { GlobalStyles } from "@mui/material";
-import {
-  type IInsetCallbackFunc,
-  type Inset,
-} from "@totalpave/cordova-plugin-insets";
-import { use, useEffect, useState } from "react";
-
-// @ts-expect-error -- something wrong with library types
-// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment,@typescript-eslint/no-unsafe-call,@typescript-eslint/no-unsafe-member-access -- something wrong with library types
-const insetPromise: Promise<Inset> = globalThis.totalpave.Inset.create();
 
 export const Insets = () => {
-  const inset = use(insetPromise);
+  if (cordova.platformId === "android") {
+    return (
+      <GlobalStyles
+        styles={{
+          ":root": {
+            "--inset-bottom": "0px",
+            "--inset-left": "0px",
+            "--inset-right": "0px",
+            "--inset-top": "0px",
+          },
+        }}
+      />
+    );
+  }
 
-  const [values, setValues] = useState(() => inset.getInset());
-
-  useEffect(() => {
-    const onChange: IInsetCallbackFunc = (inset) => {
-      setValues(inset);
-    };
-
-    inset.addListener(onChange);
-
-    return () => {
-      inset.removeListener(onChange);
-    };
-  }, [inset]);
-
-  return (
-    <GlobalStyles
-      styles={{
-        ":root": {
-          "--inset-bottom": `${values.bottom}px`,
-          "--inset-left": `${values.left}px`,
-          "--inset-right": `${values.right}px`,
-          "--inset-top": `${values.top}px`,
-        },
-      }}
-    />
-  );
+  return null;
 };
