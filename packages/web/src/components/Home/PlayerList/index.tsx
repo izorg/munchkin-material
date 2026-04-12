e0e041d184f95461aa4b180ec90794f1b92e85ca
Viacheslav Zavoruev <izorgster@gmail.com>
2026-04-12 18:41:34 +0200

    chore(web): compact sortable logic

--- packages/web/src/components/Home/PlayerList/index.tsx
+++ packages/web/src/components/Home/PlayerList/index.tsx
@@ -1,13 +1,28 @@
-import { type DragEndEvent } from "@dnd-kit/core";
-import { hasSortableData } from "@dnd-kit/sortable";
-import { type ListProps } from "@mui/material";
+import {
+  closestCenter,
+  DndContext,
+  type DragEndEvent,
+  KeyboardSensor,
+  PointerSensor,
+  useSensor,
+  useSensors,
+} from "@dnd-kit/core";
+import { restrictToVerticalAxis } from "@dnd-kit/modifiers";
+import {
+  hasSortableData,
+  SortableContext,
+  sortableKeyboardCoordinates,
+  verticalListSortingStrategy,
+} from "@dnd-kit/sortable";
+import { List, type ListProps } from "@mui/material";
 
 import { movePlayer } from "../../../ducks/playerList";
+import useEditMode from "../../../hooks/useEditMode";
+import usePresentSelector from "../../../hooks/usePresentSelector";
 import { useAppDispatch } from "../../../store";
 
-import { PlayerListDndContext } from "./PlayerListDndContext";
+import Item from "./Item";
 import { PlayerListDragOverlay } from "./PlayerListDragOverlay";
-import { PlayerSortableList } from "./PlayerSortableList";
 
 const HomePlayerList = (props: ListProps) => {
   const dispatch = useAppDispatch();
@@ -33,11 +48,35 @@ const HomePlayerList = (props: ListProps) => {
     }
   };
 
+  const playerList = usePresentSelector((state) => state.playerList);
+
+  const { editMode } = useEditMode();
+
   return (
-    <PlayerListDndContext onDragEnd={onDragEnd}>
-      <PlayerSortableList {...props} />
+    <DndContext
+      collisionDetection={closestCenter}
+      modifiers={[restrictToVerticalAxis]}
+      onDragEnd={onDragEnd}
+      sensors={useSensors(
+        useSensor(PointerSensor),
+        useSensor(KeyboardSensor, {
+          coordinateGetter: sortableKeyboardCoordinates,
+        }),
+      )}
+    >
+      <List disablePadding {...props}>
+        <SortableContext
+          disabled={!editMode}
+          items={playerList}
+          strategy={verticalListSortingStrategy}
+        >
+          {playerList.map((playerId) => (
+            <Item key={playerId} playerId={playerId} />
+          ))}
+        </SortableContext>
+      </List>
       <PlayerListDragOverlay />
-    </PlayerListDndContext>
+    </DndContext>
   );
 };
 
